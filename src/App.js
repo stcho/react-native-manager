d40787ca877c42e7c12910ce2b18adc8080b4697
Thomas Cho <s.thomas.cho@gmail.com>
2018-03-23 12:03:24 -0700

    Create emailChanged action creator

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import firebase from 'firebase';
 import reducers from './reducers';
+import LoginFrom from './components/LoginForm'
 
 class App extends Component {
 	componentWillMount() {
@@ -22,11 +23,7 @@ class App extends Component {
 	render() {
 		return (
 			<Provider store={createStore(reducers)}>
-				<View>
-					<Text>
-						Hello!
-					</Text>
-				</View>
+				<LoginFrom />
 			</Provider>
 		);
 	}
---
+++ src/actions/index.js
@@ -0,0 +1,6 @@
+export const emailChanged = () => {
+	return {
+		type: 'email_changed',
+		payload: text
+	};
+};
