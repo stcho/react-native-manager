ddd69c4b27fb9589d01d7ab3d395df0dc81e6557
Thomas Cho <s.thomas.cho@gmail.com>
2018-03-23 22:47:37 -0700

    Finished with Email input

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,8 @@
-export const emailChanged = () => {
+import { EMAIL_CHANGED } from './types';
+
+export const emailChanged = (text) => {
 	return {
-		type: 'email_changed',
+		type: EMAIL_CHANGED,
 		payload: text
 	};
 };
