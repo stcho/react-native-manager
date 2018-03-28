aceabcf85ed2154633dd31a9cf377ac30de8a8a2
Thomas Cho <s.thomas.cho@gmail.com>
2018-03-28 11:14:38 -0700

    Add redux thunk and initial login user action creator

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
-import LoginFrom from './components/LoginForm'
+import LoginFrom from './components/LoginForm';
 
 class App extends Component {
 	componentWillMount() {
@@ -21,8 +21,10 @@ class App extends Component {
 	}
 
 	render() {
+		const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+		
 		return (
-			<Provider store={createStore(reducers)}>
+			<Provider store={store}>
 				<LoginFrom />
 			</Provider>
 		);
--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,6 +1,7 @@
 import { 
 	EMAIL_CHANGED,
-	PASSWORD_CHANGED
+	PASSWORD_CHANGED,
+	LOGIN_USER_SUCCESS
 } from '../actions/types';
 
 const INITIAL_STATE = { 
@@ -14,6 +15,8 @@ export default (state=INITIAL_STATE, action) => {
 			return { ...state, email: action.payload }
 		case PASSWORD_CHANGED:
 			return { ...state, password: action.payload }
+		case LOGIN_USER_SUCCESS:
+			return { ...state, user: action.payload }
 		default:
 			return state;
 	}
