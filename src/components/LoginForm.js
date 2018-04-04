93b81c89ec72546b3d646696ffd98ade6dce94ed
Thomas Cho <s.thomas.cho@gmail.com>
2018-04-04 10:59:36 -0700

    Handle Login Success and Login Fail

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -17,6 +17,18 @@ class LoginForm extends Component {
 		this.props.loginUser({email, password});
 	}
 
+	renderError() {
+		if (this.props.error) {
+			return (
+				<View style={{backgroundColor: 'white'}}>
+					<Text style={styles.errorTextStyle}>
+						{this.props.error}
+					</Text>
+				</View>
+			);
+		}
+	}
+
 	render() {
 		return(
 			<Card>
@@ -39,6 +51,8 @@ class LoginForm extends Component {
 					/>
 				</CardSection>
 
+				{this.renderError()}
+
 				<CardSection>
 					<Button onPress={this.onButtonPress.bind(this)}>
 						Login
@@ -49,10 +63,19 @@ class LoginForm extends Component {
 	}
 }
 
+const styles = {
+	errorTextStyle: {
+		fontSize: 20,
+		alignSelf: 'center',
+		color: 'red'
+	}
+}
+
 const mapStateToProps = state => {
 	return {
 		email: state.auth.email,
-		password: state.auth.password
+		password: state.auth.password,
+		error: state.auth.error
 	};
 };
 
