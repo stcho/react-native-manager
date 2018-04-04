5d34ce1bd700f1e87933528af8327efb9d9fb7b5
Thomas Cho <s.thomas.cho@gmail.com>
2018-04-04 11:45:45 -0700

    Add loading spinner for login

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
 import { emailChanged, passwordChanged, loginUser } from '../actions'
-import { Card, CardSection, Input, Button } from './common';
+import { Card, CardSection, Input, Button, Spinner } from './common';
 
 class LoginForm extends Component {
 	onEmailChange(text) {
@@ -29,6 +30,18 @@ class LoginForm extends Component {
 		}
 	}
 
+	renderButton() {
+		if (this.props.loading) {
+			return <Spinner size='large' />;
+		}
+
+		return (
+			<Button onPress={this.onButtonPress.bind(this)}>
+				Login
+			</Button>
+		);
+	}
+
 	render() {
 		return(
 			<Card>
@@ -54,9 +67,7 @@ class LoginForm extends Component {
 				{this.renderError()}
 
 				<CardSection>
-					<Button onPress={this.onButtonPress.bind(this)}>
-						Login
-					</Button>
+					{this.renderButton()}
 				</CardSection>
 			</Card>
 		);
@@ -72,11 +83,9 @@ const styles = {
 }
 
 const mapStateToProps = state => {
-	return {
-		email: state.auth.email,
-		password: state.auth.password,
-		error: state.auth.error
-	};
+	const { email, password, error, loading } = state.auth;
+
+	return { email, password, error, loading };
 };
 
 export default connect(mapStateToProps, { 
--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,6 +1,7 @@
 import { 
 	EMAIL_CHANGED,
 	PASSWORD_CHANGED,
+	LOGIN_USER,
 	LOGIN_USER_SUCCESS,
 	LOGIN_USER_FAIL
 } from '../actions/types';
@@ -9,7 +10,8 @@ const INITIAL_STATE = {
 	email: '',
 	password: '',
 	user: null,
-	error: ''
+	error: '',
+	loading: false
 };
 
 export default (state=INITIAL_STATE, action) => {
@@ -18,10 +20,12 @@ export default (state=INITIAL_STATE, action) => {
 			return { ...state, email: action.payload }
 		case PASSWORD_CHANGED:
 			return { ...state, password: action.payload }
+		case LOGIN_USER:
+			return { ...state, loading: true, error: '' }
 		case LOGIN_USER_SUCCESS:
-			return { ...state, user: action.payload }
+			return { ...state, user: action.payload, error: '', loading: false }
 		case LOGIN_USER_FAIL:
-			return { ...state, error: 'Authentication Failed.', password: '' }
+			return { ...state, error: 'Authentication Failed.', password: '', loading: false }
 		default:
 			return state;
 	}
