d3b256a0437058326ee81cdaa0050c5e470f92de
Thomas Cho <s.thomas.cho@gmail.com>
2018-04-06 10:13:34 -0700

    Routing from Login to Employee List

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux'
 import { 
 	EMAIL_CHANGED, 
 	PASSWORD_CHANGED,
@@ -24,7 +25,7 @@ export const passwordChanged = (text) => {
 export const loginUser = ({email, password}) => {
 	return (dispatch) => {
 		dispatch({ type: LOGIN_USER });
-		
+
 		firebase.auth().signInWithEmailAndPassword(email, password)
 			.then(user => loginUserSuccess(dispatch, user))
 			.catch(() => {
@@ -37,6 +38,8 @@ export const loginUser = ({email, password}) => {
 
 const loginUserSuccess = (dispatch, user) => {
 	dispatch({ type: LOGIN_USER_SUCCESS, payload: user });
+
+	Actions.main();
 };
 
 const loginUserFail = (dispatch) => {
