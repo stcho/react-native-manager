af56a533f50be64a60dafbfff28ea8be0e70e44b
Thomas Cho <s.thomas.cho@gmail.com>
2018-04-07 13:48:32 -0700

    EmployeeCreate wip

---
+++ src/reducers/EmployeeFormReducer.js
@@ -0,0 +1,18 @@
+import {
+	EMPLOYEE_UDDATE 
+} from '../actions/types';
+
+const INITIAL_STATE = { 
+	name: '',
+	phone: '',
+	shift: ''
+};
+
+export default (state=INITIAL_STATE, action) => {
+	switch (action.type) {
+		case EMPLOYEE_UDDATE:
+			return { ...state, [action.payload.prop]: action.payload.value };
+		default:
+			return state;
+	}
+};
