daff473d11af2ae151cb4c8e31d45b874a056da5
Thomas Cho <s.thomas.cho@gmail.com>
2018-04-23 09:53:06 -0700

    inital Confirm for fire employee

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -4,9 +4,11 @@ import { connect } from 'react-redux';
 import Communications from 'react-native-communications';
 import EmployeeForm from './EmployeeForm';
 import { employeeUpdate, employeeSave } from '../actions';
-import { Card, CardSection, Button } from './common';
+import { Card, CardSection, Button, Confirm } from './common';
 
 class EmployeeEdit extends Component {
+	state = { showModal: false };
+
 	componentWillMount() {
 		_.each(this.props.employee, (value, prop) => {
 			this.props.employeeUpdate({ prop, value });
@@ -29,16 +31,30 @@ class EmployeeEdit extends Component {
 		return (
 			<Card>
 				<EmployeeForm />
+
 				<CardSection>
 					<Button onPress={this.onButtonPress.bind(this)} >
 						Save Changes
 					</Button>
 				</CardSection>
+				
 				<CardSection>
 					<Button onPress={this.onTextPress.bind(this)}>
 						Text Schedule
 					</Button> 
 				</CardSection>
+
+				<CardSection>
+					<Button onPress={() => this.setState({ showModal: !this.state.showModal })}>
+						Fire Employee
+					</Button>
+				</CardSection>
+
+				<Confirm
+					visible={this.state.showModal}
+				>
+					Are you sure you want to Fire this employee?
+				</Confirm>
 			</Card>
 		);
 	}
